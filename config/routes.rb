ab4c9a28deb4df9d7b1ea2d14529aefafcb606f8
007steve <stephen.plummer1.sp@gmail.com>
2020-05-27 23:09:28 -0400

    added tests

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+  resources :reviews
+  resources :bottles
+  devise_for :users, controllers:  {omniauth_callbacks: 'omniauth', registrations: 'registrations' }
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root to: "application#welcome"
 end
